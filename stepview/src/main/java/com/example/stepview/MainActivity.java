44d1c803295845ce8b68dae1290898d95eea15d2
jihymood <huangjihy@163.com>
2017-06-06 15:33:12 +0800

    横向滚动的，可以支持大量文本选择的 自定义View

--- stepview/src/main/java/com/example/stepview/MainActivity.java
+++ stepview/src/main/java/com/example/stepview/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.stepview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.horizontalselectedviewlibrary.HorizontalselectedView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private List<String> list;
+    private HorizontalselectedView horizontalselectedView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(i + "00");
+        }
+        horizontalselectedView = (HorizontalselectedView) findViewById(R.id.hd_main);
+        horizontalselectedView.setData(list);
+        horizontalselectedView.setAnLeftOffset();
+
     }
 }
